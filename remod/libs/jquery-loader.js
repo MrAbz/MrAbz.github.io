e6da8ecbde4816ce1a02a931ce4bbc3f0e807bc8
Mr. Abz <Mr. Abz>
2017-05-07 02:23:32 +0530

    test pop

--- remod/libs/jquery-loader.js
+++ remod/libs/jquery-loader.js
@@ -5,7 +5,7 @@
 
   /* jshint -W060 */
   if (library) {
-    document.write('<script src="remod/libs/' + library[1] + '"></script>');
+    document.write('<script src="../libs/' + library[1] + '"></script>');
   } else {
     document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>');
   }
